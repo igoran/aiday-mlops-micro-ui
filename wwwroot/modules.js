13965780feea96c6f2b05b91110a8cc90ca09322
Luca Milan <luca.milan@gmail.com>
2021-02-22 14:13:23 +0100

    Fx ping

--- wwwroot/modules.js
+++ wwwroot/modules.js
@@ -1,4 +1,10 @@
+var FX_URL = "https://fxappdev6f822e30.azurewebsites.net/";
 
 $(function(){
     console.log("start js...");
+
+    $.get( FX_URL + "api/ping", function( data ) {
+        console.log("start fx", data);
+    });
+
 });
